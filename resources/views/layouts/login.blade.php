091c732e79f139aae5d816ee71424a262792d796
Lef <terisyou@hotmail.com>
2021-04-26 22:30:48 +0300

    First Commit Client Template Implement

---
+++ resources/views/layouts/login.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>@yield('title')</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="front-end/images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="front-end/vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="front-end/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="front-end/fonts/iconic/css/material-design-iconic-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="front-end/vendor/animate/animate.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="front-end/vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="front-end/vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="front-end/vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="front-end/vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="front-end/css/util.css">
+	<link rel="stylesheet" type="text/css" href="front-end/css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+
+@yield('content')
+
+
+<!--===============================================================================================-->
+    <script src="front-end/vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+    <script src="front-end/vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+    <script src="front-end/vendor/bootstrap/js/popper.js"></script>
+    <script src="front-end/vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+    <script src="front-end/vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+    <script src="front-end/vendor/daterangepicker/moment.min.js"></script>
+    <script src="front-end/vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+    <script src="front-end/vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+    <script src="front-end/js/login.js"></script>
+
+ </body>
+</html>
